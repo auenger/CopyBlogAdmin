7a2f062e412cdb0201193c69f3030218f5028f61
yzw40459 <yzw40459@ly.com>
2018-01-02 17:46:40 +0800

    --copyblogadmin

---
+++ js/nav.js
@@ -0,0 +1,32 @@
+var navs = [{
+	"title": "公告管理",
+	"icon": "&#xe609;",
+	"spread": false,
+	"href": "pages/notice_detail.html"
+}, {
+	"title": "文章管理",
+	"icon": "fa-calendar-minus-o",
+	"spread": true,
+	"children": [{
+		"title": "所有文章",
+		"icon": "fa-list",
+		"href": "pages/article_list.html"
+	}, {
+		"title": "分类目录",
+		"icon": "fa-folder-open",
+		"href": "pages/article_list.html"
+	}, {
+		"title": "发布文章",
+		"icon": "fa-edit",
+		"href": "pages/article_list.html"
+	}, {
+		"title": "文章标签",
+		"icon": "fa-tags",
+		"href": "pages/tag_list.html"
+	}]
+}, {
+	"title": "全局设置",
+	"icon": "fa-cog",
+	"spread": false,
+	"href": "pages/article_list.html"
+}];
